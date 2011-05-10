fd287db4173aa965941b636e8f915f020f496421
Shea Rial <sheatrevor@gmail.com>
2011-05-10 13:47:15 -0400

    Created a spellbook dispatch script.

--- spellbook.sh
+++ spellbook.sh
@@ -1,7 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
-# Display a warning and prevent the spellbook from running
-echo "Don't ever try to run the spellbook directly. Copy-and-paste only what you need from it."
-exit 0
+# If no arguments were passed in
+if [ "$#" == "0" ]; then
+    # Determine the name of this script file
+    script_filename=`basename $0`
+    # Display the usage instructions for this script, then stop
+    echo "Spellbook usage: $script_filename script-name [arguments...]"
+    exit 1
+fi
 
-x="pn"; nx="png"; for f in *.$x; do b=`basename "$f" ".$x"`; echo "$b.$x - $b.$nx"; mv "$b.$x" "$b.$nx"; done # Renames all of the files in the current directory with file extension $x to have file extension $nx
+# Grab the name of the child script that we are trying to run
+child_script_name=$1
+
+# Shift the arguments one position
+shift
+
+# Determine the two spellbook locations for the current user
+directory_spellbook=`pwd`"/spellbook"
+users_spellbook=`echo ~`"/spellbook"
+users_hidden_spellbook=`echo ~`"/.spellbook"
+
+# Holds the name of the execute script files
+execute="execute.sh"
+
+# Create a list of filenames that the requested spell file could have
+possible_spell_locations=$directory_spellbook"/"$child_script_name"/"$execute" \
+    "$users_spellbook"/"$child_script_name"/"$execute" \
+    "$users_hidden_spellbook"/"$child_script_name"/"$execute
+
+# Loop over each of the possible spell locations
+for current_spell_filename in $possible_spell_locations; do
+
+    # If the current spell file exists
+    if [ -f $current_spell_filename ]; then
+        # Run it, forwarding the shifted arguments
+        bash $current_spell_filename "$@"
+        # Halt execution
+        exit 0
+    fi
+
+done
+
+# Explain to the user that the child script could not be found
+echo "The spell $child_script_name could not be found in any of the configured spellbook directories."
+exit 1
---
+++ spellbook/rename-file-extensions/execute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create meaningful variable names for the input arguments
+original_extension=$1
+new_extension=$2
+
+# Loop over all of the files in the current directory
+for filename in *.$original_extension; do
+    # Grab the filename without the original extension
+    base_filename=`basename "$filename" ".$original_extension"`
+    # Rename the file, replacing the extension with the new one
+    mv "$base_filename.$original_extension" "$base_filename.$new_extension"
+done
